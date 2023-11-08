121faff78887a0968b01596fe0c80640215281d5
tanmaykapoor <tanmay.skater@gmail.com>
2023-11-07 19:12:28 -0500

    done till 3.3.8

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import Hello from "./hello.js";
-const app = express();
+import Lab5 from "./lab5.js";
 
+const app = express();
+app.use(express.json());
+
+Lab5(app);
 Hello(app);
+
 app.listen(4000, () => console.log("Server is running on port 4000"));
