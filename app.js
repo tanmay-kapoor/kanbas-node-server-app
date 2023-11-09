8fbbec28d7193c4b319ce2fa12c0f45654769748
tanmaykapoor <tanmay.skater@gmail.com>
2023-11-09 18:09:32 -0500

    solve cors issue

--- app.js
+++ app.js
@@ -12,12 +12,24 @@ import AssignmentRoutes from "./assignments/routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
     cors({
         credentials: true,
         origin: process.env.FRONTEND_URL,
     })
 );
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
 
 const PORT = process.env.PORT || 4000;
 
